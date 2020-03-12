3a56233802e95f73aaf2dcb8f0f5a30bf1e680a8
ohead <daehok0822@gmail.com>
2020-03-12 16:48:06 +0900

    페이지네이션

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        App\Article::truncate();
+
+        factory('App\Article', 100)->create();
+    }
+}
---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        App\Category::truncate();
+
+        DB::table('categories')->insert([
+            'name'=> '소설',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+        DB::table('categories')->insert([
+            'name'=> '수필',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+        DB::table('categories')->insert([
+            'name'=> '시',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+    }
+}
