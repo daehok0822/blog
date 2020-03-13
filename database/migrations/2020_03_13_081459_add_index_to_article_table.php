3a86bc730b8ba19273eeb789968b3c7c81726efa
ohead <daehok0822@gmail.com>
2020-03-13 17:51:25 +0900

    풀텍스트 검색

---
+++ database/migrations/2020_03_13_081459_add_index_to_article_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+            $table->index('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('article', function (Blueprint $table) {
+            //
+        });
+    }
+}
