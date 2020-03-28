2bfdced2c39a91c6474b02ec56989ce2ed7d78f2
daehok <daehok0822@gmail.com>
2020-03-28 17:27:09 +0900

    일반 사용자 추가

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -25,5 +25,15 @@ class UsersTableSeeder extends Seeder
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
 
+        DB::table('users')->insert([
+            'name' => 'ohead',
+            'email' => 'ohead@blog.test',
+            'email_verified_at' => date("Y-m-d H:i:s"),
+            'password' => bcrypt('admin') , // password
+            'isAdmin' => false,
+            'remember_token' => '1234',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
     }
 }
