c7a14124640334019de9c3b56d635c2778a844ef
daehok <daehok0822@gmail.com>
2020-03-29 15:24:50 +0900

    썸네일 저장

--- app/Http/Controllers/CkeditorController.php
+++ app/Http/Controllers/CkeditorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class CkeditorController extends Controller
 {
@@ -28,18 +29,17 @@ class CkeditorController extends Controller
 
             $extension = $request->file('upload')->getClientOriginalExtension();
 
+            $thumbnailName = $fileName.'_300x300_'.time().'.'.$extension;
             $fileName = $fileName.'_'.time().'.'.$extension;
 
-
-
             $request->file('upload')->move(public_path('uploads'), $fileName);
-
-
 
             $CKEditorFuncNum = $request->input('CKEditorFuncNum');
 
             $url = asset('uploads/'.$fileName);
 
+            $img = Image::make('uploads/'.$fileName)->crop(300, 300);
+            $img->save('uploads/' . $thumbnailName, 90);
             $msg = 'Image uploaded successfully';
 
             $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
