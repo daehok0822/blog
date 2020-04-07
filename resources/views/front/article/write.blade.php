8b2b3fb1f1fe8d17aa564040eff2105300765556
ohead <daehok0822@gmail.com>
2020-04-07 16:56:53 +0900

    자바스크립트 입력 체크

--- resources/views/front/article/write.blade.php
+++ resources/views/front/article/write.blade.php
@@ -2,17 +2,18 @@
 @section('articles')
     <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
 
-    <form action="{{ route('article.store' )}}" method="post" enctype="multipart/form-data">
+    <form id="write-form" action="{{ route('article.store' )}}" method="post" enctype="multipart/form-data" onsubmit="return checkForm()">
         @csrf
-        <input type="text" name="title" placeholder="제목"></p>
-        <textarea name="description" id="editor1" name="editor1" rows="10" cols="80"></textarea>
+        <input type="text" id="title" name="title" placeholder="제목"></p>
+        <textarea name="description" id="editor1" rows="10" cols="80"></textarea>
         <script>
-            CKEDITOR.replace('editor1', {
+            var editor = CKEDITOR.replace('editor1', {
                 filebrowserUploadUrl: "{{route('ckeditor.upload', ['_token' => csrf_token() ])}}",
                 filebrowserUploadMethod: 'form'
             });
+
         </script>
-        <select name="category" >
+        <select name="category" id="category">
             <option value="0">- Select -</option>
             @foreach($categories as $category)
                 <option value="{{$category -> id}}">{{$category -> name}}</option>
@@ -31,6 +32,26 @@
             var file_attach = $('#file_attach_init').clone();
             file_attach.removeAttr('id').removeAttr("style");;
             $('#file_add').before(file_attach);
+        }
+        function checkForm() {
+            if ($('#title').val() == '') {
+                alert('제목을 입력하세요.');
+                $('#title').focus();
+                return false;
+            }
+
+            var desc = editor.getData();
+            if (desc == '') {
+                alert('본문을 입력하세요.');
+                editor.focus();
+                return false;
+            }
+            if ($('#category').val() == '0') {
+                alert('카테고리를 선택하세요.');
+                $('#category').focus();
+                return false;
+            }
+            return true;
         }
     </script>
 {{--<script>--}}
